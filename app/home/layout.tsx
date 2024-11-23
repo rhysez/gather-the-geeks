85da92ffa367e488c0b1c3ef6eab32df8af667a2
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2024-11-23 15:36:28 +0000

    add CommandLineIcon with hover animation to PrimaryNav

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -6,7 +6,7 @@ export default function HomeLayout({children}: Readonly<{
     return (
         <section>
             <PrimaryNav />
-            <main className={"p-4"}>
+            <main className={"px-2 md:px-24 py-4"}>
                 {children}
             </main>
         </section>
--- components/layout/PrimaryNav.tsx
+++ components/layout/PrimaryNav.tsx
@@ -1,18 +1,20 @@
-import {Button} from "@/components/ui/button";
 import Link from "next/link";
-import {Bars3Icon} from "@heroicons/react/24/solid";
+import {CommandLineIcon} from "@heroicons/react/24/solid";
 
 export default function PrimaryNav() {
     return (
         <nav className={"bg-lighter_background px-2 gap-4 md:gap-0 md:px-24 py-4 flex flex-col md:flex-row justify-between items-center"}>
-            <Link href={"/home"}>
-                <h2 className={"text-lightning_yellow font-bold text-2xl"}>GATHER THE GEEKS</h2>
+            <Link className={"hover:animate-bounce"} href={"/home"}>
+                <section className={"flex items-center gap-2"}>
+                    <h2 className={"text-lightning_yellow font-bold text-2xl"}>GATHER THE GEEKS!</h2>
+                    <CommandLineIcon className={"w-8 h-8 text-lightning_yellow"} />
+                </section>
             </Link>
             <section className={"space-x-6"}>
-                <Link className={"text-lightning_yellow hover:underline"} href="/home/about">Meet ups</Link>
-                <Link className={"text-lightning_yellow hover:underline"} href="/home/about">Ask a dev</Link>
-                <Link className={"text-lightning_yellow hover:underline"} href="/home/about">Blogs</Link>
-                <Link className={"text-lightning_yellow hover:underline"} href="/home/about">About GTG</Link>
+            <Link className={"text-lightning_yellow font-bold hover:underline"} href="/home/about">Meet ups</Link>
+                <Link className={"text-lightning_yellow font-bold hover:underline"} href="/home/about">Ask a dev</Link>
+                <Link className={"text-lightning_yellow font-bold hover:underline"} href="/home/about">Blogs</Link>
+                <Link className={"text-lightning_yellow font-bold hover:underline"} href="/home/about">About GTG</Link>
             </section>
         </nav>
     )
