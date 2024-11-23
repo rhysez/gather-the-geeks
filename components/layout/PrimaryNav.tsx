5e6905890b6a506e485050f51ffb6a524d90d603
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2024-11-23 14:47:02 +0000

    add Bars3 icon button and About GTG link

--- components/layout/PrimaryNav.tsx
+++ components/layout/PrimaryNav.tsx
@@ -1,9 +1,17 @@
-
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+import {Bars3Icon} from "@heroicons/react/24/solid";
 
 export default function PrimaryNav() {
     return (
-        <nav className={"bg-lighter_background px-6 py-3 flex justify-center items-center"}>
-            <h2 id={"logo-main"} className={"text-lightning_yellow font-bold text-2xl"}>GATHER THE GEEKS</h2>
+        <nav className={"bg-lighter_background px-6 py-4 flex justify-between items-center"}>
+            <Button className={"bg-background text-white border-2 hover:bg-lighter_background"}>
+                <Bars3Icon className={"text-white h-8 w-8"} />
+            </Button>
+            <h2 className={"text-lightning_yellow font-bold text-2xl"}>GATHER THE GEEKS</h2>
+            <section>
+                <Link className={"text-lightning_yellow hover:underline"} href="/home">About GTG</Link>
+            </section>
         </nav>
     )
 }
