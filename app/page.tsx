e9888fe05e9aaeb49ab22c9db84d29f13880f7ad
Rhys Hodgson <121772484+rhysez@users.noreply.github.com>
2024-12-15 13:52:00 +0000

    add root page content

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-
-
-export default async function Home() {
+export default async function Page() {
   return (
       <main>
-        <p>Main content</p>
+        <section className="space-y-2">
+            <h1 className="text-center font-extrabold lg:text-5xl md:text-3xl text-2xl">
+                start meeting <span className="text-[#f87171]">like-minded</span> people<span className="text-cherry_red">.</span>
+            </h1>
+            <p className="text-center opacity-75 lg:text-xl md:text-base text-sm">
+                gather the geeks helps your find and collaborate with other devs<span className="text-cherry_red">.</span>
+            </p>
+        </section>
       </main>
   );
 }
